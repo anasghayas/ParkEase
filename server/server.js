ec5a2920f2a33844d7c981ebddb3a7c76c7037ce
anasghayas <anasghayas2004@gmail.com>
2026-07-23 00:11:36 +0530

    Built User model and backend authentication APIs

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', require('./routes/authRoutes'));
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB ParkEase'))
   .catch(err => console.error('MongoDB connection error:', err));
