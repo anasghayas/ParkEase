c42c448381f6a993f169e64e3dc0fa5ae681081c
anasghayas <anasghayas2004@gmail.com>
2026-07-24 15:40:21 +0530

    Cloudinary upload

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/slots', require('./routes/slotRoutes'));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB ParkEase'))
